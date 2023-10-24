b254af7a2759adab4c71b4a7a4d0c00d3d3eb114
Paco <pacors88@gmail.com>
2023-10-24 07:18:38 +0200

    first commit

---
+++ 02-comments/06-comments.ts
@@ -0,0 +1,10 @@
+
+/**
+  * Tips: Shift + Alt + A
+  *  Toggle block comment
+  */
+
+const fruitsComment = ['banana', 'pear', /* 'orange' */, 'strawberry'];
+
+// Goal: comment the 'orange' string as the previous example
+const fruitsExercise = ['banana', 'pear', 'orange', 'strawberry'];
---
+++ 04-definitions/definitions.ts
@@ -0,0 +1,13 @@
+/**
+ * Hover greet
+ * Hover greet with Crtl
+ * F12 -> go to definition
+ * Alt + F12 -> look definition
+ * Open new file + Crtl + W to close (same as google chrome)
+ */
+
+import { greet } from "./functions";
+
+const greeting = greet('Chayanne');
+
+console.log(greeting);
