cc94aa75c7194f24d88ae41ea3af717af538b5ef
Paco Rodríguez <francisco.rodriguez@icareweb.com>
2023-10-27 09:12:30 +0200

    change multicursor copy example, unify file examples and add README

---
+++ README.md
@@ -0,0 +1,38 @@
+# VSCode from Hero to Zero
+
+## Section 1
+
+- Code command
+- Recommended extensions
+
+## Section 2
+
+- Movements with cursor
+- Comments
+- Files
+- Definitions
+- Delete
+- Zen mode & Sidebar
+- Terminal
+- Emmet wrap
+- Tabs management
+- Tabulation
+
+## Section 3
+
+- Clone lines
+- Basic multi cursor
+- Multi cursor copy
+- Multi cursor format
+- Capitalize
+- Multi cursor (different positions)
+- Next Match
+- Array
+
+## Section 4
+
+- Definitions
+- Go to line
+- Makrdown
+- Replace definitions
+- Snippets
---
+++ Section-3/03-V2multi-cursor-copy.ts
@@ -0,0 +1,55 @@
+
+//*
+//*     Check add cursor above
+//*     Tips: Shift + Alt ↑ / ↓
+//* 
+
+
+`
+Spectrum plot // Zoom X-axis function (keyboard shortcut)
+Spectrum plot // Zoom Y-axis function (keyboard shortcut)
+Waveform plot // Zoom X-axis function (keyboard shortcut)
+Waveform plot // Zoom Y-axis function (keyboard shortcut)
+Trend plot // New float cursor with date; time; value reference
+Cursor functions // New Micro step function
+Cursor functions // New fault marks function
+Cursor functions // New find peak function
+`
+
+// Final example
+const releaseNotes = [
+  {
+    "type": "New",
+    "description": "Spectrum plot // Zoom X-axis function (keyboard shortcut)",
+  },
+  {
+    "type": "New",
+    "description": "Spectrum plot // Zoom Y-axis function (keyboard shortcut)",
+  },
+  {
+    "type": "New",
+    "description": "Waveform plot // Zoom X-axis function (keyboard shortcut)",
+  }  ,
+  {
+    "type": "New",
+    "description": "Waveform plot // Zoom Y-axis function (keyboard shortcut)",
+  },
+  {
+    "type": "New",
+    "description": "Trend plot // New float cursor with date; time; value reference",
+  },
+  {
+    "type": "New",
+    "description": "Cursor functions // New Micro step function",
+  },
+  {
+    "type": "New",
+    "description": "Cursor functions // New fault marks function",
+  },
+  {
+    "type": "New",
+    "description": "Cursor functions // New find peak function",
+  },
+]
+
+// Goal: create the release notes using multi cursor
